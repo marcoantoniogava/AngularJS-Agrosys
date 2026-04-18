d67396cb1af3dc82ee4334a5e0948bef1855a0b7
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2026-04-18 00:00:57 -0300

    Ajustes em Promises

--- Estudos/javascript/script.js
+++ Estudos/javascript/script.js
@@ -1,4 +1,4 @@
-const p1 = new Promise((resolve) => {
+/* const p1 = new Promise((resolve) => {
     setTimeout(() => resolve("final"), 1000);
 });
 
@@ -6,4 +6,22 @@ const p2 = new Promise((resolve) => {
     resolve(p1);
 });
 
-p2.then(console.log);
+p2.then(console.log); */
+
+/* 
+const p1 = new Promise(res => res(console.log))
+
+p1.then((callback) => {callback(123)}) 
+*/
+
+const asyncFunction = async () => {
+    console.log("teste")
+    console.log(await new Promise(res => {
+        setTimeout(() => {
+            res("123")
+        }, 1000);
+    }));
+    console.log("teste2")
+}
+
+asyncFunction();
