2136c28628075f17c11beda33a4aa4ada0e2972a
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2026-04-21 23:19:20 -0300

    Aprofudando estudo sobre prototypes

---
+++ Estudos/javascript/Scripts testes/Exemplo prototype.js
@@ -0,0 +1,26 @@
+function Produto (nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+}
+
+Produto.prototype.aumentarPreco = function (valor) {
+    this.preco += valor;
+}
+
+Produto.prototype.diminuiPreco = function (valor) {
+    this.preco -= valor;
+}
+
+function Camiseta (nome, preco, material) {
+    Produto.call(this, nome, preco);
+    this.material = material;
+}
+
+Camiseta.prototype = Object.create(Produto.prototype);
+Camiseta.prototype.constructor = Camiseta;
+
+const produto = new Produto('Prod. Generico', 10);
+const camiseta = new Camiseta('Regata', 57.5, 'algodão');
+
+console.log(produto);
+console.log(camiseta);
