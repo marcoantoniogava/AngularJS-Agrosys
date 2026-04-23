0326e44b9f3dc6c9c9e841fddeaf45ba5f48a041
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2026-04-22 22:29:45 -0300

    Ajustes gerais nos estudos

--- Estudos/javascript/Scripts testes/Exemplo prototype.js
+++ Estudos/javascript/Scripts testes/Exemplo prototype.js
@@ -22,5 +22,7 @@ Camiseta.prototype.constructor = Camiseta;
 const produto = new Produto('Prod. Generico', 10);
 const camiseta = new Camiseta('Regata', 57.5, 'algodão');
 
+camiseta.aumentarPreco(100);
+
 console.log(produto);
 console.log(camiseta);
