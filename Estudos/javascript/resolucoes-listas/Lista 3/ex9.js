d643d53868bb1eaf1ed8c1145a892359c76bc280
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2026-04-28 14:55:28 -0300

    ex 1 ao 8 lista 3

---
+++ Estudos/javascript/resolucoes-listas/Lista 3/ex9.js
@@ -0,0 +1,13 @@
+/* 9. (simulando Promise.any)
+implemente uma função simples estilo Promise.any:
+function promiseAny(promises) {
+  // sua implementação
+}
+regras:
+retorna o valor da primeira promise que resolver
+ignora as que falharem
+se todas falharem, retorna erro */
+
+function promiseAny(promises) {
+//FAZER    
+}
