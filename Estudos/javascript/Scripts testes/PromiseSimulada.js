e8cfb2274c1d5619c6e6b241afb32e15f9b324a4
Nícolas Machado Cardoso <nicolasxzx1225@gmail.com>
2026-04-30 17:49:50 -0300

    Adicionando reject na Promise Simulada

--- Estudos/javascript/Scripts testes/PromiseSimulada.js
+++ Estudos/javascript/Scripts testes/PromiseSimulada.js
@@ -1,60 +1,100 @@
 class PromiseSimulada {
     constructor(executor) {
-        let callbacks = [];
+        let callbacksResolve = [];
+        let callbacksReject = [];
         let estado = "pending";
         let valorPromise;
 
         function resolveSimulado(valor) {
-            if (estado == "fullfiled") return;
+            if (estado !== "pending") return;
 
             estado = "fullfiled";
             valorPromise = valor;
 
             setTimeout(() => {
-                callbacks.forEach(cb => {
+                callbacksResolve?.forEach(cb => {
+                    cb(valorPromise);
+                });
+            }, 0);
+        }
+
+        function rejectSimulado(valor) {
+            if (estado !== "pending") return;
+
+            estado = "rejected";
+            valorPromise = valor;
+
+            setTimeout(() => {
+                callbacksReject?.forEach(cb => {
                     cb(valorPromise);
                 });
             }, 0);
         }
 
         this.then = function (callback) {
-            return new PromiseSimulada((res) => {
+            return new PromiseSimulada((res, rej) => {
                 function handleResolve () {
-                    let resultado = callback(valorPromise)
+                    if (estado == "rejected")
+                        return rej(valorPromise);
                     
-                    res(resultado)
+                    let resultado = callback(valorPromise);
+
+                    res(resultado);
                 }
 
-                if (estado == "pending")
-                    callbacks.push(() => { handleResolve(); });
+                if (estado == "pending") 
+                    [callbacksResolve, callbacksReject].forEach(arr => {
+                        arr.push(handleResolve)
+                    });
+                
+                else if (estado == "rejected")
+                    rej(valorPromise);
 
                 else setTimeout(() => {
                     handleResolve();
+                }, 0);
+            });
+        };
+
+        this.catch = function (callback) {
+            return new PromiseSimulada((res) => {
+                function handleReject () {
+                    let resultado = callback(valorPromise);
+
+                    res(resultado);
+                }
+
+                if (estado == "pending")
+                    callbacksReject.push(() => { handleReject(); });
+
+                else setTimeout(() => {
+                    handleReject();
                 }, 0);
                     
             });
         };
 
-        executor(resolveSimulado);
+        executor(resolveSimulado, rejectSimulado);
     }
 
     static resolve(valor) {
         return new PromiseSimulada(res => res(valor));
     }
+
+    static reject(valor) {
+        return new PromiseSimulada((res, rej) => rej(valor));
+    }
 }
 
 /* EXEMPLOS */
 
-const p = new PromiseSimulada((res) => {
+const p = new PromiseSimulada((res, rej) => {
     setTimeout(() => {
-        res(2)
+        rej(2)
     }, 1000);
 });
 
-p.then(x => x + 2)
- .then(x => x * 2)
- .then(console.log);
-
-PromiseSimulada.resolve(2).then(console.log);
-
-console.log("sync");
+p.then(x=>x*2)
+ .then(x=>x+1)
+ .then(x=>console.log("não deveria rodar"))
+ .catch(console.log);
